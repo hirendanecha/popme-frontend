05678280e5655385440d51e5afee85ea271df586
hirendanecha <hirendanecha@gmail.com>
2023-03-10 12:29:19 +0530

    add space

--- src/features/workspaces/index.jsx
+++ src/features/workspaces/index.jsx
@@ -14,6 +14,7 @@ import Button from "../../components/Button/Button";
 
 const Workspaces = () => {
   const dispatch = useDispatch();
+  
 
   const [workspacePosts, setWorkspacePosts] = useState([]);
 
