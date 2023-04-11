4b0d1370e45e4f856231fea1c057e9977eee07f4
hiren.danecha <hirendanecha@gmail.com>
2023-04-11 18:05:42 +0530

    changes in instant embed

--- src/features/customization/WorkspaceOptions/InstantEmbed.jsx
+++ src/features/customization/WorkspaceOptions/InstantEmbed.jsx
@@ -129,9 +129,32 @@ const InstantEmbed = ({ register }) => {
                         {`${item?.url}`}
                       </div>
                       <div className="collapse-content">
+                        <label className="label mt-2">
+                          <span
+                            className={`label-text text-primary-main text-base font-semibold`}
+                          >
+                            In this website
+                          </span>
+
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth="1.5"
+                            stroke="currentColor"
+                            className="w-5 h-5 text-primary-main"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                            />
+                          </svg>
+                        </label>
+
                         <div className="inline-block w-full">
                           <SelectBox
-                            labelTitle="In this website"
+                            // labelTitle="In this website"
                             labelStyle="text-primary-main text-base font-semibold"
                             options={[
                               {
@@ -140,7 +163,7 @@ const InstantEmbed = ({ register }) => {
                               },
                               { name: "Option 2", value: "option2" },
                             ]}
-                            containerStyle="mt-2 mb-3 w-full"
+                            containerStyle="mb-3 w-full"
                             selectStyle="text-primary-main"
                             name="instantEmbed.inThisWebsite"
                             // register={register}
