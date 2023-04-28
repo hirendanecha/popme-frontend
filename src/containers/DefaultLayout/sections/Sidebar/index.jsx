40f5f7ff8200ead642186e85a775570b51ee8bb0
KtanPatel <ktan.p.patel@gmail.com>
2023-04-28 18:12:25 +0530

    fix: google login fix

--- src/containers/DefaultLayout/sections/Sidebar/index.jsx
+++ src/containers/DefaultLayout/sections/Sidebar/index.jsx
@@ -43,11 +43,9 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
 
         if (Object.keys(res?.selectedPlan).length === 0) {
           navigate("/app/setting", { state: { tab: 2 } });
+        } else {
+          navigate("/app/workspaces");
         }
-
-        // else {
-        //   navigate("/app/workspaces");
-        // }
 
         // if (
         //   !(
